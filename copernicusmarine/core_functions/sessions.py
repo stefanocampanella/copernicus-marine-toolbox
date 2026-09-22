dba2d6a3ddc0d6e4979e99cfc8f4306f7baab5d7
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2026-09-22 11:28:53 +0200

    Quick and dirty fix of mercator-ocean/copernicus-marine-toolbox/issues/509

--- copernicusmarine/core_functions/sessions.py
+++ copernicusmarine/core_functions/sessions.py
@@ -10,6 +10,7 @@ import requests
 import requests.auth
 from boto3.s3.transfer import TransferConfig
 from requests.adapters import HTTPAdapter, Retry
+from os import cpu_count
 
 from copernicusmarine.core_functions.environment_variables import (
     COPERNICUSMARINE_DISABLE_SSL_CONTEXT,
@@ -63,6 +64,7 @@ def get_configured_boto3_session(
     config_boto3 = botocore.config.Config(
         signature_version=botocore.UNSIGNED,
         retries={"max_attempts": 10, "mode": "adaptive"},
+        max_pool_connections=cpu_count() if cpu_count() else 10,
     )
     s3_session = boto3.Session()
     s3_client = s3_session.client(
